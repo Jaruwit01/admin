8f06c4303a15661bc6b5000d5117c9e7a41ef8c7
จารุวิทย์ สิงห์โสม <86384340+Jaruwit01@users.noreply.github.com>
2024-05-09 01:40:08 +0700

    up

--- src/page/user.js
+++ src/page/user.js
@@ -1,6 +1,4 @@
-// user.js is a simple component that fetches data from the server and displays it.
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "../components/navbar";
@@ -18,6 +16,7 @@ function User() {
             console.error(error);
         }
     };
+
     useEffect(() => {
         if (!check) {
             fetchUser();
@@ -25,38 +24,45 @@ function User() {
         }
     }, [check]);
 
+    // Function to format date as dd/mm/yyyy
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        const day = date.getDate().toString().padStart(2, '0');
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    };
+
     return (
         <div>
             <Navbar />
-        <div className="container mt-5 border-1 border ">
-        <div className="contrainer m-2">
-          <table className="table text-center ">
-            <thead>
-              <tr>
-                <th scope="col">Email</th>
-                <th scope="col">Name</th>
-                <th scope="col">Phonenumber</th>
-                <th scope="col">Date of Birth</th>
-              </tr>
-            </thead>
-            <tbody>
-            
-              {user &&
-                user.map((user, index) => (
-                  <tr key={index}>
-                    <td>{user.Email}</td>
-                    <td>{user.Name}</td>
-                    <td>{user.Phonenumber}</td>
-                    <td>{user.DateofBirth}</td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
+            <div className="container mt-5 border-1 border ">
+                <div className="container m-2">
+                    <table className="table text-center ">
+                        <thead>
+                            <tr>
+                                <th scope="col">Email</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Phonenumber</th>
+                                <th scope="col">Date of Birth</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {user &&
+                                user.map((user, index) => (
+                                    <tr key={index}>
+                                        <td>{user.Email}</td>
+                                        <td>{user.Name}</td>
+                                        <td>{user.Phonenumber}</td>
+                                        <td>{formatDate(user.DateofBirth)}</td>
+                                    </tr>
+                                ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-        </div>
-        </div>
-        
-      );
+    );
 }
 
 export default User;
